be8b60e4c424cbd4ef1fc4ca3a0b169f16e00791
Nameless1004 <jjho1029@gmail.com>
2024-08-09 10:43:19 +0900

    init project

---
+++ src/test/java/com/sparta/springasignment/SpringAsignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springasignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAsignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
